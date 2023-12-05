168cc2aa86497da6e0fbdf1370865f5473573eab
Idehed <>
2023-12-05 11:49:11 +0000

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,10 +8,12 @@ document.addEventListener("DOMContentLoaded", function () {
             }
             else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         });
     }
+
+    runGame("addition");
 });
 
 
@@ -19,9 +21,16 @@ document.addEventListener("DOMContentLoaded", function () {
  * The main game "loop", called when script is first loaded
  * and after user's answer has been processed
  */
-function runGame() {
+function runGame(gameType) {
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`unknown game type: ${gameType}`);
+        throw `unknown game type: ${gameType}.Aborting!`;
+    }
 
 }
 
@@ -41,7 +50,11 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
+
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "+";
 
 }
 
